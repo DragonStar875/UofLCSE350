38cbd69d93b18b2274b0a486d3f9d1e94e4b224e
Spencer Frederick Klink <spencer.klink@louisville.edu>
2025-06-23 10:39:36 -0400

    updated get_nutrition with correct column names

--- get_nutrition.py
+++ get_nutrition.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
+
 userPantry = pd.read_csv('userPantry.csv')
 
 def get_nutrition(userPantry, food_name):
-    return userPantry.loc[userPantry['item_name'] == food_name]
-
+    return userPantry.loc[userPantry['item_name'] == food_name ['food_name','protein', 'fat', 'calories', 'carbohydrates', 'sugar']]
 
