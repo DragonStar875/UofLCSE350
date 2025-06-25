2b23ae2524322c99cecad99ae073c9c5d1bd9e8d
itsnebby <nebulousttv@gmail.com>
2025-06-24 21:58:58 -0400

    Completed skeleton for pantry query.

--- get_nutrition.py
+++ get_nutrition.py
@@ -5,4 +5,3 @@ userPantry = pd.read_csv('userPantry.csv')
 
 def get_nutrition(userPantry, food_name):
     return userPantry.loc[userPantry['item_name'] == food_name ['food_name','protein', 'fat', 'calories', 'carbohydrates', 'sugar']]
-
