4fee020d046e1e2ca1832ea8c41bd7e0bdd454c1
itsnebby <nebulousttv@gmail.com>
2025-07-02 10:34:42 -0400

    Accidentally working in main, switching branches.

--- source_queries/pantryQuery.py
+++ source_queries/pantryQuery.py
@@ -4,6 +4,6 @@ userPantry = pd.read_csv('userPantry.csv')
 
 def pantryQuery(userPantry, food_name):
     result = userPantry.loc[userPantry['item_name'] == food_name, ['quantity', 'critical_threshold']]
-        if result.empty:
-            return "Item cannot be found"
+    if result.empty:
+        return "Item cannot be found"
     return result
