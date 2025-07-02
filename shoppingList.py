1b3f870ee4de7fc30179bc395add506775aade91
jtlen <jtlenn01@louisville.edu>
2025-07-02 12:28:23 -0400

    Shopping List class with both addItem and removeItem (unsure how we want to search for items with removeItem, but I just did it by item_name)

---
+++ shoppingList.py
@@ -0,0 +1,23 @@
+import csv
+
+import pandas as pd
+
+userPantry = pd.read_csv('userPantry.csv')
+shoppingList = pd.read_csv('shoppingList.csv')
+allGroceries = pd.read_csv('allGroceries.csv')
+
+def addItem(shoppingList, item_name, price):
+    with open(shoppingList, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([item_name, price])
+
+def removeItem(filename, item_name):
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        rows = [row for row in reader if row[0] != item_name]
+
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(header)
+        writer.writerows(rows)
