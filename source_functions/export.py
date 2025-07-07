1b3f677b0268e3706a8376b38a8dbd6b9ab11303
DragonStar875 <nbpowers2004@gmail.com>
2025-07-06 20:44:03 -0400

    Added export functionality

---
+++ source_functions/export.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sys
+
+# overwrite dataframe to csv's
+shoppingList = pd.read_csv('../shoppingList.csv')
+
+
+# export shopping list to export.csv
+def exitProgram(shoppingList):
+    try:
+        shoppingList.to_csv("export.csv", index=False)
+        print("Shopping list exported!")
+    except Exception as e:
+        print(f"Error exporting: {e}")
+    sys.exit()
