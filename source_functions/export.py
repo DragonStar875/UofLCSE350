4f3b813bf31e7dd7c1876eceaef1560cce859708
DragonStar875 <nbpowers2004@gmail.com>
2025-07-06 20:47:42 -0400

    Cleaned up

--- source_functions/export.py
+++ source_functions/export.py
@@ -6,9 +6,9 @@ shoppingList = pd.read_csv('../shoppingList.csv')
 
 
 # export shopping list to export.csv
-def exitProgram(shoppingList):
+def exportShoppingList():
     try:
-        shoppingList.to_csv("export.csv", index=False)
+        shoppingList.to_csv('../export.csv', index=False)
         print("Shopping list exported!")
     except Exception as e:
         print(f"Error exporting: {e}")
