66fa443b9c9aa2b2f1ca3b639c60af3a2b05642a
ztface01 <ztface01@louisville.edu>
2025-07-08 12:20:15 -0400

    Update priceQuery.py

--- source_functions/priceQuery.py
+++ source_functions/priceQuery.py
@@ -7,11 +7,11 @@ userPantry = pd.read_csv('userPantry.csv')
 allGroceries = pd.read_csv('allGroceries.csv')
 
 def get_price(allGroceries, food_name):
-    result = allGroceries.loc[allGroceries['item_name'] == food_name, ['food_name', 'quantity', 'price']]
+    result = allGroceries.loc[allGroceries['item_name'] == food_name, ['food_name','quantity', 'price']]
     if result.empty:
         return None
 
     price = result.iloc[0]['price']
     if pd.isna(price):
         return None
-    return price
+    return result
