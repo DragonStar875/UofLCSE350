8fd23e77926d0a755225d4bb18608f97e38a2330
ztface01 <ztface01@louisville.edu>
2025-07-11 01:21:59 -0400

    Update test_add_items.py

--- tests/test_add_items.py
+++ tests/test_add_items.py
@@ -1,10 +1,12 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import pytest
 import pandas as pd
 from pantry_utils import update_user_pantry, get_user_pantry
+
 
 @pytest.fixture
 def userPantry():
@@ -12,10 +14,11 @@ def userPantry():
         'item_name': ['butter', 'milk', 'tomato'],
         'quantity': [4, 2, 5],
         'exp_date': ['2025-07-11', '2026-10-01', '2027-02-03'],
-        'threshold': [1, 1, 1] 
+        'threshold': [1, 1, 1]
     }
-  
+
     return pd.DataFrame(mock_database)
+
 
 @pytest.fixture
 def allGroceries():
@@ -27,6 +30,17 @@ def allGroceries():
     }
 
     return pd.DataFrame(mock_database)
+
+def test_input_search_found(userPantry):
+    query = "tomato"
+    matches = userPantry[userPantry['item_name'].str.contains(query, case=False)]
+    assert not matches.empty
+    assert 'tomato' in matches['item_name'].values
+
+def test_input_search_not_found(userPantry):
+    query = "pudding"
+    matches = userPantry[userPantry['item_name'].str.contains(query, case=False)]
+    assert matches.empty
 
 def test_add_new_item(userPantry, allGroceries):
     result = update_user_pantry(userPantry, allGroceries, 'pasta', 2)
@@ -44,5 +58,5 @@ def test_add_existing_item(userPantry, allGroceries):
     milk_entry = next(item for item in pantry_dict if item['item_name'].lower() == 'milk')
 
     assert result.get("success") is not None
-    assert milk_entry['quantity'] == 6 
+    assert milk_entry['quantity'] == 6
     # 2(initial) + 1 + 3 = 6
